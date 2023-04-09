85cce9bc9debfff33cd7f5a00bb59768a70928c2
cwmTi3n <hokimtien2002@gmail.com>
2023-04-09 09:29:06 +0700

    Signup

--- app/src/main/java/com/app/shopdodientu/activity/SignupActivity.java
+++ app/src/main/java/com/app/shopdodientu/activity/SignupActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,7 @@ import com.app.shopdodientu.R;
 import com.app.shopdodientu.api.client.ApiClient;
 import com.app.shopdodientu.api.service.ApiService;
 import com.app.shopdodientu.model.UserModel;
+import com.google.android.material.textfield.TextInputEditText;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,7 +25,9 @@ public class SignupActivity extends AppCompatActivity {
     private TextView tvLogin;
     private EditText edtEmail;
     private EditText edtUsername;
-    private EditText edtPassword;
+    private TextInputEditText tiedPassword;
+    private TextInputEditText tiedtConfirm;
+    private EditText edtPhone;
     private Button btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +42,10 @@ public class SignupActivity extends AppCompatActivity {
         tvLogin = (TextView) findViewById(R.id.tvlogin);
         edtEmail = (EditText) findViewById(R.id.edtemail);
         edtUsername = (EditText) findViewById(R.id.edtusername);
-        edtPassword = (EditText) findViewById(R.id.edtpass);
-        btnSignup = (Button) findViewById(R.id.btnlogin);
+        tiedPassword = (TextInputEditText) findViewById(R.id.tfpass);
+        tiedtConfirm = (TextInputEditText) findViewById(R.id.tfconfirmpass);
+        edtPhone = (EditText) findViewById(R.id.edtphone);
+        btnSignup = (Button) findViewById(R.id.btnsignup);
     }
     private void gotoLogin() {
         tvLogin.setOnClickListener(new View.OnClickListener() {
@@ -52,20 +58,28 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     private void Signup() {
-        String email = edtEmail.getText().toString();
-        String username = edtUsername.getText().toString();
-        String password = edtPassword.getText().toString();
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String email = edtEmail.getText().toString();
+                String username = edtUsername.getText().toString();
+                String password = tiedPassword.getText().toString();
+                String confirm = tiedtConfirm.getText().toString();
+                String phone = edtPhone.getText().toString();
+                if(!password.equals(confirm)) {
+                    Toast.makeText(SignupActivity.this, "Mật khẩu không khớp", Toast.LENGTH_LONG).show();
+                    return;
+                }
                 ApiService apiService = ApiClient.getApiService();
-                apiService.signup(username, password, email, "0000000000")
+                apiService.signup(username, password, email, phone)
                         .enqueue(new Callback<UserModel>() {
                             @Override
                             public void onResponse(Call<UserModel> call, Response<UserModel> response) {
                                 UserModel userModel = response.body();
                                 if(userModel != null) {
                                     Toast.makeText(SignupActivity.this, "Đăng ký tài khoản thành công", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                                    startActivity(intent);
                                 }
                                 else {
                                     Toast.makeText(SignupActivity.this, "Đăng ký tài thất bại", Toast.LENGTH_LONG).show();
