c57cc71ffe8e8e996fbbe7be6562069e0a83ff25
LyHuong0404 <97493531+LyHuong0404@users.noreply.github.com>
2023-04-15 17:48:18 +0700

    Create UI activity_my_order

---
+++ app/src/main/java/com/app/shopdodientu/activity/MyOrderActivity.java
@@ -0,0 +1,16 @@
+package com.app.shopdodientu.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.shopdodientu.R;
+
+public class MyOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_order);
+    }
+}
