730d26ac96082a21250f138b1eabf1b2c1cfbe1c
LyHuong0404 <97493531+LyHuong0404@users.noreply.github.com>
2023-04-29 23:51:05 +0700

    Create UI Order Infor

---
+++ app/src/main/java/com/app/shopdodientu/activity/CancelOrderInforActivity.java
@@ -0,0 +1,16 @@
+package com.app.shopdodientu.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.shopdodientu.R;
+
+public class CancelOrderInforActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cancel_order_infor);
+    }
+}
