387fdc92c54fd4ab0ba0fab3401ee2c19d33d18c
LyHuong0404 <97493531+LyHuong0404@users.noreply.github.com>
2023-05-02 15:24:57 +0700

    Create UI CheckOut and Address

---
+++ app/src/main/java/com/app/shopdodientu/activity/ChooseAddressActivity.java
@@ -0,0 +1,16 @@
+package com.app.shopdodientu.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.shopdodientu.R;
+
+public class ChooseAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_address);
+    }
+}
