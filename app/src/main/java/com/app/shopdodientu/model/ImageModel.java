6c50f7a826dc4322a1e124d9975fe321d8db4396
LyHuong0404 <97493531+LyHuong0404@users.noreply.github.com>
2023-05-05 18:45:50 +0700

    Add SliderImage

---
+++ app/src/main/java/com/app/shopdodientu/model/ImageModel.java
@@ -0,0 +1,19 @@
+package com.app.shopdodientu.model;
+
+public class ImageModel {
+    private  int imageId;
+
+    public ImageModel(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+
+}
