41ab88482719c9863f79c3eb05f03c722aca6730
LyHuong0404 <97493531+LyHuong0404@users.noreply.github.com>
2023-05-12 00:12:23 +0700

    Start ChooseAddressActivity

--- app/src/main/java/com/app/shopdodientu/activity/ChooseAddressActivity.java
+++ app/src/main/java/com/app/shopdodientu/activity/ChooseAddressActivity.java
@@ -3,7 +3,9 @@ package com.app.shopdodientu.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.app.shopdodientu.R;
@@ -18,11 +20,22 @@ public class ChooseAddressActivity extends AppCompatActivity {
         setContentView(R.layout.activity_choose_address);
 
         MapItemView();
+        TextViewAddClicked();
     }
 
     private void MapItemView(){
         tvBack = findViewById(R.id.tvBack);
-        tvAddAddress = findViewById(R.id.tvAddress);
+        tvAddAddress = findViewById(R.id.tvAddAddress);
         rcvAddress = findViewById(R.id.rcvAddress);
+    }
+
+    private void TextViewAddClicked(){
+        tvAddAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseAddressActivity.this, LocationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
