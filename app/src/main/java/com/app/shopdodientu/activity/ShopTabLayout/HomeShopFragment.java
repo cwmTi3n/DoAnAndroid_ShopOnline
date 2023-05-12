49ee9eb69eaaf2c3d62030e3de01ef5a42c6d688
cwmTi3n <hokimtien2002@gmail.com>
2023-05-12 10:55:08 +0700

    Commit

--- app/src/main/java/com/app/shopdodientu/activity/ShopTabLayout/HomeShopFragment.java
+++ app/src/main/java/com/app/shopdodientu/activity/ShopTabLayout/HomeShopFragment.java
@@ -8,13 +8,34 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.app.shopdodientu.R;
+import com.app.shopdodientu.adapter.ProductAdapter;
+import com.app.shopdodientu.api.client.ApiClient;
+import com.app.shopdodientu.api.service.ApiService;
 import com.app.shopdodientu.databinding.FragmentHomeshopBinding;
 import com.app.shopdodientu.databinding.FragmentWardBinding;
+import com.app.shopdodientu.model.PageModel;
+import com.app.shopdodientu.model.ProductModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeShopFragment extends Fragment {
+    private int sellerId;
     FragmentHomeshopBinding binding;
-    public HomeShopFragment(){}
+    private RecyclerView rcvRecommend, rcvLastProduct;
+    private ProductAdapter recommendProductAdapter, lastproductAdapter;
+    private List<ProductModel> recommendProducts, lastProducts;
+    public HomeShopFragment(int sellerId){
+        this.sellerId = sellerId;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -26,8 +47,57 @@ public class HomeShopFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         binding = FragmentHomeshopBinding.inflate(inflater, container, false);
-
+        rcvRecommend = binding.getRoot().findViewById(R.id.rcvRecommend);
+        rcvLastProduct = binding.getRoot().findViewById(R.id.rcvLastProduct);
         //vi tri load du lieu: recyclerview
         return binding.getRoot();
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        renderView();
+    }
+
+    private void renderView() {
+
+        renderProduct("descamount");
+        renderProduct("desccreateDate");
+    }
+
+    private void renderProduct(String orderby) {
+        ApiService apiService = ApiClient.getApiService();
+        apiService.findProductsBySeller(sellerId, 0, orderby)
+                .enqueue(new Callback<PageModel<ProductModel>>() {
+                    @Override
+                    public void onResponse(Call<PageModel<ProductModel>> call, Response<PageModel<ProductModel>> response) {
+                        PageModel<ProductModel> page = response.body();
+                        if(page!= null) {
+                            if(orderby.equals("descamount")) {
+                                recommendProducts = page.getContent();
+                                recommendProductAdapter = new ProductAdapter(binding.getRoot().getContext(), recommendProducts);
+                                rcvRecommend.setHasFixedSize(true);
+                                rcvRecommend.setLayoutManager(new LinearLayoutManager(binding.getRoot().getContext(), LinearLayoutManager.HORIZONTAL, false));
+                                rcvRecommend.setAdapter(recommendProductAdapter);
+                                recommendProductAdapter.notifyDataSetChanged();
+                            }
+                            else if(orderby.equals("desccreateDate")) {
+                                lastProducts = page.getContent();
+                                lastproductAdapter = new ProductAdapter(binding.getRoot().getContext(), lastProducts);
+                                rcvLastProduct.setHasFixedSize(true);
+                                rcvLastProduct.setLayoutManager(new GridLayoutManager(binding.getRoot().getContext(), 2));
+                                rcvLastProduct.setAdapter(lastproductAdapter);
+                                lastproductAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<PageModel<ProductModel>> call, Throwable t) {
+
+                    }
+                });
+    }
+
+
 }
--- app/src/main/java/com/app/shopdodientu/activity/ShopTabLayout/ShopPagerAdapter.java
+++ app/src/main/java/com/app/shopdodientu/activity/ShopTabLayout/ShopPagerAdapter.java
@@ -12,9 +12,11 @@ import com.app.shopdodientu.activity.ShopTabLayout.ProductFragment;
 
 
 public class ShopPagerAdapter extends FragmentStateAdapter {
+    private int sellerId;
 
-    public ShopPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+    public ShopPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle, int sellerId) {
         super(fragmentManager, lifecycle);
+        this.sellerId = sellerId;
     }
 
     @NonNull
@@ -22,15 +24,17 @@ public class ShopPagerAdapter extends FragmentStateAdapter {
     public Fragment createFragment(int position) {
         switch (position){
             case 0:
-                return new HomeShopFragment();
+                return new HomeShopFragment(sellerId);
             case 1:
                 return new ProductFragment();
             case 2:
                 return new CatalogFragment();
             default:
-                return new HomeShopFragment();
+                return new HomeShopFragment(sellerId);
         }
     }
+
+
 
     @Override
     public int getItemCount() {
