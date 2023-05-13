021a8861d5f9b2a8ef126b8dc107c246d9087ace
LyHuong0404 <97493531+LyHuong0404@users.noreply.github.com>
2023-05-13 14:53:34 +0700

    Create Function Forget Pass

--- app/src/main/java/com/app/shopdodientu/api/client/ApiClient.java
+++ app/src/main/java/com/app/shopdodientu/api/client/ApiClient.java
@@ -5,13 +5,11 @@ import android.util.Log;
 import com.app.shopdodientu.api.service.ApiService;
 
 public class ApiClient extends BaseClient{
-<<<<<<< HEAD
-    private static final String BASE_URL = "http://192.168.1.10/";
-//    private static final String BASE_URL = "https://apishoponline.cfapps.ap21.hana.ondemand.com/";
-=======
-//    private static final String BASE_URL = "http://192.168.1.12/";
+
+//    private static final String BASE_URL = "http://192.168.1.10/";
     private static final String BASE_URL = "https://apishoponline.cfapps.ap21.hana.ondemand.com/";
->>>>>>> 852897caf9141a9675598f131c1ee87fd55e45f6
+
+
     public static ApiService apiService;
 
     public static ApiService getApiService() {
